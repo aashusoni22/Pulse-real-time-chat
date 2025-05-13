0a99b29323d0f563f39a83211cff6f8f1fd4ec93
Aashutosh Soni <omsoni051@gmail.com>
2025-05-13 13:53:15 -0400

    added .env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,13 +7,13 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4UJ2IMmeXN85zTZUr4pDVM2piaj2wV9s",
-  authDomain: "pulse-chat-975ea.firebaseapp.com",
-  projectId: "pulse-chat-975ea",
-  storageBucket: "pulse-chat-975ea.firebasestorage.app",
-  messagingSenderId: "362617827682",
-  appId: "1:362617827682:web:ec26e929620a51e9e7bc3b",
-  measurementId: "G-C0LZVK3DGX",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.ITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
